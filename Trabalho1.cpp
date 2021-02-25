606df09b4c1d342e842cc57aeeab6ce84d620bf8
João Teixeira <44053859+jteixeira00@users.noreply.github.com>
2021-02-25 19:21:18 +0000

    compress and merge left

--- Trabalho1.cpp
+++ Trabalho1.cpp
@@ -4,12 +4,71 @@
 #include <unordered_map>
 #include <list>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
+void printMatrix(vector<vector<int>> matrix, int size)
+{
+    cout << "\n";
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
 
-int main() {
-// We probably do not need this but it is faster
+vector<vector<int>> compressLeft(vector<vector<int>> matrix, int size)
+{
+    vector<vector<int>> new_matrix(size, vector<int>(size));
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            new_matrix[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+        int last_pos = 0;
+        for (int j = 0; j < size; j++)
+        {
+            if (matrix[i][j] != 0)
+            {
+                new_matrix[i][last_pos] = matrix[i][j];
+                last_pos++;
+            }
+        }
+    }
+
+    return new_matrix;
+}
+
+vector<vector<int>> mergeLeft(vector<vector<int>> matrix, int size)
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size - 1; j++)
+        {
+            if ((matrix[i][j] == matrix[i][j + 1]) && (matrix[i][j] != 0))
+            {
+                matrix[i][j] = matrix[i][j] + matrix[i][j + 1];
+                matrix[i][j + 1] = 0;
+            }
+        }
+    }
+    return matrix;
+}
+
+int main()
+{
+    // We probably do not need this but it is faster
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     string order;
     int size;
     //int key;
@@ -18,34 +77,40 @@ int main() {
     int num;
     int maxSlides;
 
-    int posx = 0;
-    int posy = 0;
+    int x = 0;
+    int y = 0;
 
     cin >> max;
 
-    while(max != count){        
+    while (max != count)
+    {
 
         cin >> size >> maxSlides;
-
-        while(posy != size){
+        vector<vector<int>> matrix(size, vector<int>(size));
+        while (y != size)
+        {
 
             cin >> num;
-
-            cout << posx << " " << posy << '\n';
-
-            if(posx != size - 1){
-                ++posx;
+            matrix[y][x] = num;
+            if (x != size - 1)
+            {
+                ++x;
             }
-            else{
-                ++posy;
-                posx = 0;
+            else
+            {
+                ++y;
+                x = 0;
             }
-
         }
-        posx = posy = 0;
-        count++;
-        
-    }       
 
-    return 0;    
+        matrix = compressLeft(matrix, size);
+        matrix = mergeLeft(matrix, size);
+        cout << "output left";
+        printMatrix(matrix, size);
+
+        x = y = 0;
+        count++;
+    }
+
+    return 0;
 }
